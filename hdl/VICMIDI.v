0a80b0d3bc6ca802f67a0b481e42d22e884857b5
go4retro <go4retro@go4retro.com>
2018-05-06 15:58:35 -0500

    Working Verilog

--- hdl/VICMIDI.v
+++ hdl/VICMIDI.v
@@ -19,55 +19,93 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-module vicmidi(reset, clock, r_w, address, data, uart_en, mem_en, io, io_sel, flash_ce, ram_ce, we, oe, uart_ce, ram1,ram2,ram3, blk1, blk2, blk3, blk5, base, bank, midi_txd, midi_rxd, rs232_txd, rs232_rxd, txd, rxd, ser_sel, uart_irq, irq, nmi, uart_reset, led, switch);
-inout reset;
-input clock;
-input r_w;
-input [9:0]address;
-inout [7:0]data;
-input uart_en;
-input mem_en;
-input [3:2] io;
-input io_sel;
-output flash_ce;
-output ram_ce;
-output uart_ce;
-output we;
-output oe;
-input ram1;
-input ram2;
-input ram3;
-input blk1;
-input blk2;
-input blk3;
-input blk5;
-input [6:0]base;
-output [6:0]bank;
+module vicmidi(
+					reset, 
+					clock, 
+					r_w, 
+					address, 
+					data, 
+					uart_en, 
+					mem_en, 
+					io, 
+					io_sel, 
+					flash_ce, 
+					ram_ce, 
+					we, 
+					oe, 
+					uart_ce, 
+					ram1,
+					ram2,
+					ram3, 
+					blk1, 
+					blk2, 
+					blk3, 
+					blk5, 
+					base, 
+					bank, 
+					midi_txd, 
+					midi_rxd, 
+					rs232_txd, 
+					rs232_rxd, 
+					txd, 
+					rxd, 
+					ser_sel, 
+					uart_irq, 
+					irq, 
+					nmi, 
+					uart_reset, 
+					act_led, 
+					switch
+				  );
 
-output  midi_txd;
-input   midi_rxd;
-output  rs232_txd;
-input   rs232_rxd;
-input   txd;
-output  rxd;
-input   ser_sel;
-input   uart_irq;
-output  irq;
-output  nmi;
-output  uart_reset;
+inout 	reset;
+input 	clock;
+input 	r_w;
+input 	[9:0]address;
+inout 	[7:0]data;
+input 	uart_en;
+input 	mem_en;
+input 	[3:2] io;
+input 	io_sel;
+output 	flash_ce;
+output 	ram_ce;
+output 	uart_ce;
+output 	we;
+output 	oe;
+input 	ram1;
+input 	ram2;
+input 	ram3;
+input 	blk1;
+input 	blk2;
+input 	blk3;
+input 	blk5;
+input 	[6:0]base;
+output 	[5:0]bank;
 
-output led;
-input [1:0]switch;
+output 	act_led;
+input 	[1:0]switch;
+output  	irq;
+output  	nmi;
 
-wire [7:0] reg_data;
-wire reg_data_read;
-wire reset_out;
-wire data_read;  // we don't use this...
+output  	midi_txd;
+input   	midi_rxd;
+output  	rs232_txd;
+input   	rs232_rxd;
+input   	txd;
+output  	rxd;
+input   	ser_sel;
+input   	uart_irq;
+output  	uart_reset;
 
-//wire is_uart_addr_valid;
+wire 		led;
+wire 		[7:0] reg_data;
+wire 		reg_data_read;
+wire 		reset_out;
+wire 		data_read;  // we don't use this...
 
 // assigns
-assign io_uart = 						(io_sel ? io[3] : io[2]);
+//assign io_uart = 						(io_sel ? io[3] : io[2]);
+assign io_uart = 						io[3];
 
 assign midi_txd = 					(ser_sel ? 0 : txd);
 assign rs232_txd = 					(ser_sel ? txd : rs232_rxd);
@@ -75,18 +113,16 @@ assign rxd = 							(ser_sel ? rs232_rxd : midi_rxd);
 
 assign irq = 							(uart_irq ? 0 : 1'bz);  // negate IRQ from RS232
 assign nmi = 							1'bz;
-//assign reg_base = !(base);
-assign reg_base = 0;
 assign uart_reset = 					!reset;
-assign uart_ce = 						(!uart_en & !io_uart & (address[9:3] == 7'b0)); // active high
-//assign uart_ce = (!io_uart & is_uart_addr_valid); // active high
+assign uart_ce = 						(!uart_en & !io_uart & (address[9:3] == base[6:0])); // active high
 
+assign act_led =                 (led ? 0 : 1'bz);
 assign reset =                	(!reset_out ? 0 : 1'bz);  // reset_out is active low
 
 assign data = 							(reg_data_read ? reg_data : 8'bz);
 
 
-MemExpander #(.WIDTH(6))			MemExpander1((mem_en & !uart_ce),  // on except when uart is accessed.
+MemExpander #(.WIDTH(6), .MEMEXPANDER_ID('b00010010))			MemExpander1((!mem_en & !uart_ce),  // on except when uart is accessed.
                                               reset, 
                                               reset_out, 
 															 clock, 
@@ -112,7 +148,5 @@ MemExpander #(.WIDTH(6))			MemExpander1((mem_en & !uart_ce),  // on except when 
 															 switch,
 															 data_read
 															);
-
-//comparator #(.WIDTH(7))	uart_address_comp(address[9:3], reg_base, is_uart_addr_valid);
 
 endmodule
--- hdl/register.v
+++ hdl/register.v
@@ -19,8 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef _register_v
-`define _register_v
-
+`define _register_v
 module register(clock, reset, enable, d, q);
 
 parameter WIDTH = 8 ;
@@ -32,6 +31,7 @@ input enable;
 input [WIDTH-1:0] d;
 output [WIDTH-1:0] q;
 reg [WIDTH-1:0] q;
+initial q = RESET;
 
 always @ (negedge clock, posedge reset)
   begin
@@ -40,29 +40,5 @@ always @ (negedge clock, posedge reset)
   else if(enable)
 	   q <= d;
   end
-endmodule
-
-
-module register2(clock, reset, enable, reset_d, d, q);
-
-parameter WIDTH = 8 ;
-
-input clock;
-input reset;
-input enable;
-input [WIDTH-1:0] reset_d;
-input [WIDTH-1:0] d;
-output [WIDTH-1:0] q;
-reg [WIDTH-1:0] q;
-
-always @ (negedge clock, posedge reset)
-  begin
-  if(reset)
-		q <= reset_d;
-  else if(enable)
-	   q <= d;
-  end
-endmodule
-
-
+endmodule
 `endif
