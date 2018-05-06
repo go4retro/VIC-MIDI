533850fa770fd8b2875c79c1d3bcc0151a0412b9
go4retro <go4retro@go4retro.com>
2018-05-06 15:57:11 -0500

    More Verilog updates

--- hdl/register.v
+++ hdl/register.v
@@ -19,7 +19,8 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef _register_v
-`define _register_v
+`define _register_v
+
 module register(clock, reset, enable, d, q);
 
 parameter WIDTH = 8 ;
@@ -39,5 +40,29 @@ always @ (negedge clock, posedge reset)
   else if(enable)
 	   q <= d;
   end
-endmodule
+endmodule
+
+
+module register2(clock, reset, enable, reset_d, d, q);
+
+parameter WIDTH = 8 ;
+
+input clock;
+input reset;
+input enable;
+input [WIDTH-1:0] reset_d;
+input [WIDTH-1:0] d;
+output [WIDTH-1:0] q;
+reg [WIDTH-1:0] q;
+
+always @ (negedge clock, posedge reset)
+  begin
+  if(reset)
+		q <= reset_d;
+  else if(enable)
+	   q <= d;
+  end
+endmodule
+
+
 `endif
